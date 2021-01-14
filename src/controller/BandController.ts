64cd8d8bec281df4e1506241c825b785629d5cab
Alice <alicepsouzaba1998@gmail.com>
2021-01-14 11:10:04 -0300

    band entity insertion of date in the bank

---
+++ src/controller/BandController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { BandInputDTO } from "../model/Band";
+import { bandBusiness } from "../business/BandBusiness";
+import { BaseDatabase } from "../data/BaseDatabase";
+
+class BandController {
+  async createBand(req: Request, res: Response) {
+    try {
+      const input: BandInputDTO = {
+        name: req.body.name,
+        mainGenre: req.body.mainGenre,
+        responsible: req.body.responsible
+      }
+      await bandBusiness.createBand(input, req.headers.authorization as string)
+      res.status(200).send({
+        message: "Banda cadastrada com sucesso!"
+      });
+    } catch (error) {
+      res.status(error.customErrorCode || 400).send({
+        message: error.message
+      });
+    } finally {
+      await BaseDatabase.destroyConnection();
+    };
+  };
+};
+
+export const bandController = new BandController();
